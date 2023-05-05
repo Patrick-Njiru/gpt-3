d82690eedabbc1424bcf8f687a187815d5696f69
Patrick-Njiru <patricknjiru93@gmail.com>
2023-05-06 02:06:25 +0300

    add and style the blog component with the help of the reusable article component. Rename images.

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './article.css'
 
-const Article = () => {
-  return (
-    <div>Article</div>
+const Article = ({ imgURL, date, title }) => (
+    <div className='article'>
+      <div className='article-image'>
+        <img src={imgURL} alt='blog-image' />
+      </div>
+      <div className='article-content'>
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
-}
 
 export default Article
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { Article } from '../../components'
+import { blog1, blog2, blog3, blog4, blog5} from './imports'
 import './blog.css'
+
+const BLOGSDATA = [
+  {
+    image: blog2,
+    date: 'Sep 26, 2021',
+    title: 'GPT-3 and Open AI is the future. Let us explore how it is?'
+  },
+  {
+    image: blog3,
+    date: 'Sep 26, 2021',
+    title: 'GPT-3 and Open AI is the future. Let us explore how it is?'
+  },
+  {
+    image: blog4,
+    date: 'Sep 26, 2021',
+    title: 'GPT-3 and Open AI is the future. Let us explore how it is?'
+  },
+  {
+    image: blog5,
+    date: 'Sep 26, 2021',
+    title: 'GPT-3 and Open AI is the future. Let us explore how it is?'
+  }
+]
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='blog section__padding' id='blog'>
+      <div className='blog-heading'>
+        <h1 className='gradient__text'>
+          A lot is happening. We are blogging about it.
+        </h1>
+      </div>
+      <div className='blog-container'>
+        <div className='blog-groupA'>
+          <Article imgURL={blog1} date={BLOGSDATA[0].date} title={BLOGSDATA[0].title} />
+        </div>
+        <div className='blog-groupB'>
+          { BLOGSDATA.map((data, i) => <Article imgURL={data.image} date={data.date} title={data.title} key={i} />) }
+        </div>
+      </div>
+    </div>
   )
 }
 
