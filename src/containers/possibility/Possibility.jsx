4b206ad5d1ce81124745e1ea212159f1b858eeb0
Patrick-Njiru <patricknjiru93@gmail.com>
2023-05-06 00:19:21 +0300

    add and style the possibility component.

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './possibility.css'
+import possibility from '../../assets/possibility.png'
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='possibility section__padding' id='possibility'>
+      <div className='possibility-image'>
+        <img src={possibility} alt='possibility' />
+      </div>
+      <div className='possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, aperiam. Accusantium quo perspiciatis aperiam eos quos ex quasi quaerat libero culpa. Neque optio autem ullam ducimus unde. Commodi, quidem ipsum.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+
+    </div>
   )
 }
 
