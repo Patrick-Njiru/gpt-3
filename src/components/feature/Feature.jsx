d870aae893e3bd48e6c964037653f8c36586201a
Patrick-Njiru <patricknjiru93@gmail.com>
2023-05-05 22:36:24 +0300

    add the whatGPT3 component with the help of the feature reusable component.

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import './feature.css'
+import React from 'react';
+import './feature.css';
 
-const Feature = () => {
-  return (
-    <div>Feature</div>
-  )
-}
+const Feature = ({ title, text }) => (
+  <div className="feature-container">
+    <div className="feature-title">
+      <div />
+      <h1>{title}</h1>
+    </div>
+    <div className="feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
 
-export default Feature
+export default Feature;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './whatGPT3.css'
+import { Feature } from '../../components'
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='whatgpt3 section__margin' id='wgpt3'>
+      <div className='whatgpt3-feature'>
+        <Feature 
+          id='whatgpt3-feature'
+          title='What is GPT-3' 
+          text='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, provident accusantium? Accusantium repellat repudiandae labore distinctio tenetur ab voluptas iusto? Ducimus blanditiis laudantium harum, tempora dolor laboriosam sit cum nobis voluptatem! Quisquam corporis facilis recusandae eligendi eos cumque unde saepe.' 
+        />
+      </div>   
+      <div className='whatgpt3-heading'>
+        <h1 className='gradient__text'> The possibilities are beyond your imagination </h1>
+        <p> Explore The Library </p>
+      </div>
+      <div className='whatgpt3-container'>
+        <Feature 
+          title='Chatbots' 
+          text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea repellat, enim eos repellendus magni officia doloremque quod id in porro et nam molestias saepe commodi excepturi, ab blanditiis rerum voluptates.' 
+        />
+        <Feature 
+          title='Knowledgebase' 
+          text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea repellat, enim eos repellendus magni officia doloremque quod id in porro et nam molestias saepe commodi excepturi, ab blanditiis rerum voluptates.' 
+        />
+        <Feature 
+          title='Education' 
+          text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea repellat, enim eos repellendus magni officia doloremque quod id in porro et nam molestias saepe commodi excepturi, ab blanditiis rerum voluptates.' 
+        />
+      </div>
+    </div>
   )
 }
 
