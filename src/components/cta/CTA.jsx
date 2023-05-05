3eb560ceef8a1db43221e12056e91aab00610409
Patrick-Njiru <patricknjiru93@gmail.com>
2023-05-06 00:48:56 +0300

    add and style the CTA component.

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='cta section__margin'>
+      <div className='cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possibilities.</h3>
+      </div>
+      {/* <div className='cta-btn'> */}
+        <button type='button'>Get Started</button>
+      {/* </div> */}
+    </div>
   )
 }
 
